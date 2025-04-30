63871518d326f62259f986f2b39965490c6831e7
Kevin Ramdath <kkramdath@gmail.com>
2025-04-30 11:08:39 -0400

    add list of libaries to scrape

---
+++ src/config/libraries.py
@@ -0,0 +1,8 @@
+# src/config/libraries.py
+LIBRARIES = [
+    # "https://nmmi.overdrive.com",
+    "https://uhls.overdrive.com",
+    "https://trac.overdrive.com",
+    "https://livebrary.overdrive.com",
+    "https://lcpl.overdrive.com",
+]
