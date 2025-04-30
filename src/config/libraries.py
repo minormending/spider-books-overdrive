dca4320d7d47b42210bb74f0713bc00d4e03295a
Kevin Ramdath <kkramdath@gmail.com>
2025-04-30 11:35:07 -0400

    undo testing code

--- src/config/libraries.py
+++ src/config/libraries.py
@@ -1,6 +1,6 @@
 # src/config/libraries.py
 LIBRARIES = [
-    # "https://nmmi.overdrive.com",
+    "https://nmmi.overdrive.com",
     "https://uhls.overdrive.com",
     "https://trac.overdrive.com",
     "https://livebrary.overdrive.com",
